f5fa584ec66f8b7808f2295dae2ce18554a2e523
Micharl Farrell <mikerochip@hotmail.com>
2020-08-01 16:58:59 +0100

    Added 2 axis joypad/digital joystick

--- Arcade_Spinner.ino
+++ Arcade_Spinner.ino
@@ -1,4 +1,4 @@
-/*   Arcade Spinner v0.7
+/*   Arcade Spinner v0.71
 *    Copyright 2018 Joe W (jmtw000 a/t gmail.com)
 *                   Craig B - Updated code for mouse movement modes(DROP, ACCM) and case statement for Button port bit validation
 *    
@@ -20,18 +20,28 @@
 #include <Joystick.h>
 
                  
-#define pinA 2    //The pins that the rotary encoder's A and B terminals are connected to.
+#define pinA 2    // The pins that the rotary encoder's A and B terminals are connected to.
 #define pinB 3
-#define maxBut 6  //The number of buttons you are using up to 10.
+#define maxBut 10 // The number of buttons you are using up to 20.
 
 
 //Create a Joystick object.
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
-  maxBut, 0,             // Button Count, Hat Switch Count
-  true, true, false,    // X, Y, but no Z Axis. We need at least two axes even though they're used.
-  false, false, false,   // No Rx, Ry, or Rz
-  false, false,          // No rudder or throttle
-  false, false, false);  // No accelerator, brake, or steering;
+Joystick_ Joystick(
+	JOYSTICK_DEFAULT_REPORT_ID,		// 
+	JOYSTICK_TYPE_GAMEPAD,			// Joystick type: gamepad / digital joystick
+	maxBut,							// Button Count
+	0,             					// Hat Switch Count
+	true,							// X Axis. We need at least two axes. 
+	true,							// Y Axis. Second axis used.
+	false,							// No Z Axis  
+	false, 							// No Rx
+	false, 							// No Ry
+	false,   						// No Rz
+	false, 							// No rudder
+	false,    						// No throttle      
+	false, 							// No accelerator
+	false, 							// No brake
+	false);  						// No steering
 
 
 //The previous state of the AB pins
@@ -42,65 +52,110 @@ volatile int rotPosition = 0;
 
 volatile int rotMulti = 0;
 
+    //Sets the initial last joypad states
+    int lastJoyState[4] = {1,1,1,1};
+     
+    //Set the initial last state of the buttons depending on the max number of buttons defined in maxBut
+     
+    #if maxBut==1
+    int lastButtonState[maxBut] = {1};
+    #endif
+     
+    #if maxBut==2
+    int lastButtonState[maxBut] = {1,1};
+    #endif
+     
+    #if maxBut==3
+    int lastButtonState[maxBut] = {1,1,1};
+    #endif
+     
+    #if maxBut==4
+    int lastButtonState[maxBut] = {1,1,1,1};
+    #endif
+     
+    #if maxBut==5
+    int lastButtonState[maxBut] = {1,1,1,1,1};
+    #endif
+     
+    #if maxBut==6
+    int lastButtonState[maxBut] = {1,1,1,1,1,1};
+    #endif
+     
+    #if maxBut==7
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1};
+    #endif
+     
+    #if maxBut==8
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1};
+    #endif
+     
+    #if maxBut==9
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1};
+    #endif
+     
+    #if maxBut==10
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1};
+    #endif
 
-//Set the initial last state of the buttons depending on the max number of buttons defined in maxBut
+    #if maxBut==11
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==12
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==13
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==14
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==15
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==16
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==17
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
 
-#if maxBut==1
-int lastButtonState[maxBut] = {1};
-#endif
+    #if maxBut==18
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
+    
+    #if maxBut==19
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
 
-#if maxBut==2
-int lastButtonState[maxBut] = {1,1};
-#endif
-
-#if maxBut==3
-int lastButtonState[maxBut] = {1,1,1};
-#endif
-
-#if maxBut==4
-int lastButtonState[maxBut] = {1,1,1,1};
-#endif
-
-#if maxBut==5
-int lastButtonState[maxBut] = {1,1,1,1,1};
-#endif
-
-#if maxBut==6
-int lastButtonState[maxBut] = {1,1,1,1,1,1};
-#endif
-
-#if maxBut==7
-int lastButtonState[maxBut] = {1,1,1,1,1,1,1};
-#endif
-
-#if maxBut==8
-int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1};
-#endif
-
-#if maxBut==9
-int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1};
-#endif
-
-#if maxBut==10
-int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1};
-#endif
+    #if maxBut==20
+    int lastButtonState[maxBut] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    #endif
 
 void setup() {
   //No need to set the pin modes with DDRx = DDRx | 0b00000000 as we're using all input and that's the initial state of the pins
   //Use internal input resistors for all the pins we're using  
   PORTD = 0b11010011; //Digital pins D2, D3, D4, D6, and D12.
-  PORTB = 0b11110000; //Digital pins D8, D9, D10, D11
-  //PORTB = 0b01110010; //Digital pins D8, D9, D10, and D15 Pro Micro Craig B
+  PORTB = 0b11111111; //Digital pins D8, D9, D10, D11, MISO, MOSI, SCLK, SS
   PORTC = 0b11000000; //Digital pin D5 and D13
   PORTE = 0b01000000; //Digital pin D7
-  //PORTF = 0b11000000; //Digital pin A0 & A1
+  PORTF = 0b11110011; //Digital pin A0, A1, A2, A3, A4, A5
   
   //Start the joystick
   Joystick.begin();
-  
+     
+  // Set Range for digital joystick / joypad ie on/off.
+  Joystick.setXAxisRange(-1, 1);
+  Joystick.setYAxisRange(-1, 1);
+        
   //Center the X and Y axes on the joystick
-  Joystick.setXAxis(511);
-  Joystick.setYAxis(511);
+  Joystick.setXAxis(0);
+  Joystick.setYAxis(0);
   
   //Set up the interrupt handler for the encoder's A and B terminals on digital pins 2 and 3 respectively. Both interrupts use the same handler.
   attachInterrupt(digitalPinToInterrupt(pinA), pinChange, CHANGE); 
@@ -153,21 +208,71 @@ void pinChange() {
 
 void loop(){ 
 
-  int currentButtonState;
-
-
-  //If the encoder has moved 1 or more transitions move the mouse in the appropriate direction 
-  //and update the rotPosition variable to reflect that we have moved the mouse. The mouse will move 1/2 
-  //the number of pixels of the value currently in the rotPosition variable. We are using 1/2 (rotPosition>>1) because the total number 
-  //of transitions(positions) on our encoder is 2400 which is way too high. 1200 positions is more than enough.
-  
-  if(rotPosition >= 1 || rotPosition <= -1) {
-    rotMulti = rotPosition>> 1;                 //copy rotPosition/2 to a temporary variable in case there's an interrupt while we're moving the mouse 
-    Mouse.move(rotMulti,0,0);
-    rotPosition -= (rotMulti<< 1);              //adjust rotPosition to account for mouse movement
-  }
-
-
+      int currentButtonState;
+      int currentJoyState;
+     
+      //If the encoder has moved 1 or more transitions move the mouse in the appropriate direction 
+      //and update the rotPosition variable to reflect that we have moved the mouse. The mouse will move 1/2 
+      //the number of pixels of the value currently in the rotPosition variable. We are using 1/2 (rotPosition>>1) because the total number 
+      //of transitions(positions) on our encoder is 2400 which is way too high. 1200 positions is more than enough.
+      
+      if(rotPosition >= 1 || rotPosition <= -1) {
+        rotMulti = rotPosition>> 1;                 //copy rotPosition/2 to a temporary variable in case there's an interrupt while we're moving the mouse 
+        Mouse.move(rotMulti,0,0);
+        rotPosition -= (rotMulti<< 1);              //adjust rotPosition to account for mouse movement
+      }
+     
+      //Iterate through the 4 axis button presses (0-4) assigning the current state of the pin for each button, HIGH(0b00000001) or LOW(0b00000000), to the currentState variable
+      int jb = 0;
+       
+      do {
+        switch ( jb ) {
+          case 0:  //on digital pin A0, PF7 - Joystick Up
+            currentJoyState = (PINF & 0b10000000) >> 7; //logical AND the 8-bit pin reading with a mask to isolate the specific bit we're interested in and then shift it to the end of the byte
+            break;
+          case 1:  //on digital pin A1, PF6 - Joystick Right
+            currentJoyState = (PINF & 0b01000000) >> 6;
+            break;
+          case 2:  //on digital pin A2, PF5 - Joystick Down
+            currentJoyState = (PINF & 0b00100000) >> 5;
+            break;
+          case 3:  //on digital pin A3, PF4 - Joystick Left
+            currentJoyState = (PINF & 0b00010000) >> 4;
+            break;
+          default: //should never happen
+            currentJoyState = 0b00000000;
+            break;
+         }
+        if(currentJoyState != lastJoyState[jb]) {
+        //add code here for what you want to happen when the axis state has changed
+        
+             
+    switch ( jb ) {
+     
+        case 0: // Joystick Up
+        Joystick.setYAxis(-!currentJoyState);
+        break;
+     
+        case 2: // Joystick Down
+        Joystick.setYAxis(!currentJoyState);
+        break;
+     
+        case 1: // Joystick Right
+        Joystick.setXAxis(!currentJoyState);
+        break;
+     
+        case 3: // Joystick Left
+        Joystick.setXAxis(-!currentJoyState);
+        break;
+     
+        }
+     
+    }
+    //Save the last state for each axis for next time
+    lastJoyState[jb] = currentJoyState;
+     
+    ++jb;
+    } while (jb < 4); 
 
   //Iterate through the 10 buttons (0-9) assigning the current state of the pin for each button, HIGH(0b00000001) or LOW(0b00000000), to the currentState variable
   int button = 0;
@@ -191,52 +296,22 @@ void loop(){
       case 5:  //on digital pin 9, PB5 - Arcade Button 5
         currentButtonState = (PINB & 0b00100000) >> 5;
         break;
-      case 6: //on digital pin 10, PB6 - Arcade Button 6
+      case 6:  //on digital pin 10, PB6 - Arcade Button 6
         currentButtonState = (PINB & 0b01000000) >> 6;
         break;
-      case 7: //on digital pin 11, PB7 - Arcade Button 7
+      case 7:  //on digital pin 11, PB7 - Arcade Button 7
         currentButtonState = (PINB & 0b10000000) >> 7;
         break;
-      case 8: //on digital pin 12, PD6 - Arcade Button 8
+      case 8:  //on digital pin 12, PD6 - Arcade Button 8
         currentButtonState = (PIND & 0b01000000) >> 6;
         break;
-      case 9: //on digital pin 13, PC7 - Arcade Button 9
+      case 9:  //on digital pin 13, PC7 - Arcade Button 9
         currentButtonState = (PINC & 0b10000000) >> 6;
         break;
       default: //should never happen
         currentButtonState = 0b00000000;
         break;
 
-    /*Pro Micro 10 buttons (2 unused) Craig B
-     * switch ( button ) {
-      case 0:  //on digital pin 4, PD4 - Arcade Button 1
-        currentButtonState = (PIND & 0b00010000) >> 4;
-        break;
-      case 1:  //on digital pin 5, PC6 - Arcade Button 2
-        currentButtonState = (PINC & 0b01000000) >> 6;
-        break;
-      case 2:  //on digital pin 6, PD7 - Arcade Button 3
-        currentButtonState = (PIND & 0b10000000) >> 7;
-        break;
-      case 3:  //on digital pin 7, PE6 - Arcade Button 4
-        currentButtonState = (PINE & 0b01000000) >> 6;
-        break;
-      case 4:  //on digital pin 8, PB4 - Arcade Button 5
-        currentButtonState = (PINB & 0b00010000) >> 4;
-        break;
-      case 5:  //on digital pin 9, PB5 - Arcade Button 6
-        currentButtonState = (PINB & 0b00100000) >> 5;
-        break;
-      case 8:  //on digital pin 10, PB6 - COIN/Select Button 9
-        currentButtonState = (PINB & 0b01000000) >> 6;
-        break;
-      case 9:  //on digital pin 15, PB1 - PLAYER/Start Button 10
-        currentButtonState = (PINB & 0b00000010) >> 1;
-        break; 
-      default: //Extra digital pins 16, PB2 and 14, PB3
-        currentButtonState = 0b00000000;
-        break;
-       */
     }
     //If the current state of the pin for each button is different than last time, update the joystick button state
     if(currentButtonState != lastButtonState[button])
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Arcade-Spinner v0.7
-This is an Arduino Micro (or clone) arcade spinner with 6 buttons (up to 10) for use with MAME or any other emulator which can use the X axis of the mouse as a paddle/spinner controller. This code should also work on any board which uses the ATmega32U4 as long as the port pins are mapped to the same "digital pins" as the Micro. I created this spinner because I wanted a cheaper alternative to the commercially available ones. I find it works well for ball and paddle games, Tempest, and also makes a decent controller for driving games.
+# Arcade-Spinner v0.71
+This is an Arduino Micro (ATMega32U4) (or clone) arcade spinner with 2 axis digital joystick and 10 buttons for use with MAME or any other emulator which can use the X axis of the mouse as a paddle/spinner controller. This code should also work on any board which uses the ATmega32U4 as long as the port pins are mapped to the same "digital pins" as the Micro. I created this spinner because I wanted a cheaper alternative to the commercially available ones. I find it works well for ball and paddle games, Tempest, and also makes a decent controller for driving games.
 
- To construct this you will need a 2-phase rotary encoder which can operate at 5v and some momentary switch buttons. The rotary encoder I used is: https://www.amazon.com/Signswise-Incremental-Encoder-Dc5-24v-Voltage/dp/B00UTIFCVA 
+To construct this you will need a 2-phase rotary encoder which can operate at 5v and some momentary switch buttons. The rotary encoder I used is: https://www.amazon.com/Signswise-Incremental-Encoder-Dc5-24v-Voltage/dp/B00UTIFCVA 
 However, any 2-phase 5v one should work. You can even use the little 20-30 position encoders that come with a lot of Arduino kits, although you'll have to alter the code and I don't recommend using those. You will also need the Arduino joystick library available at: https://github.com/MHeironimus/ArduinoJoystickLibrary
 
 This device will be detected as both a mouse and a joystick/gamepad. The joystick only has X and Y axes which we don't use. At least one axis is needed by the library to declare the joystick and it was found that 2 were required for the controller to be detected on some systems. We just center the X and Y axes in setup() and forget about them. The spinner controls the mouse X axis which is by default mapped to the analog dial in MAME (don't forget to enable the mouse in MAME under advanced options!). The buttons will work as regular gamepad/joystick buttons. The 2400 different positions (transitions) that can be detected on the 600ppr(pulse per revolution) encoder I'm using are way too many for our purposes so they are halved in the code to 1200. The code uses the Atmega32u4 ports directly because that's faster than using digitalRead/Write. I'm not doing any debouncing of the buttons or encoder as it seems to work great for me as is, but you might want to add debouncing depending on your hardware.
@@ -14,13 +14,19 @@ You should now be able to use the info found at http://wiki.arcadecontrols.com/i
 
 
 
+2020-08-01
 
-7-18-2019
+Added 2 axis joystick.
+Removed Arduino Pro Micro comments.
+
+
+
+2019-07-18
 
 No more mouse movement modes, you shouldn't need them. Also added a Y axis to the controller as it was reported it was not being detected on RetroPie without it. 
 
 
 
-7-3-2019
+2019-07-03
 
 Craig B contributed some code for optimizing button polling and for different mouse movement modes (ACCM/DROP). There is now also a maxBut #define to choose how many buttons you want, up to 10, and #if logic to set the initial state of those buttons. Adding more buttons if you need them or adapting the code to your specific board should be trivial.
